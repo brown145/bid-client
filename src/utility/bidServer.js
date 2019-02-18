e7484c808df29d8210352505d07fa75532e32f56
Scott Brown <sbrown@richrelevance.com>
2019-02-18 09:50:07 -0800

    typo

--- src/utility/bidServer.js
+++ src/utility/bidServer.js
@@ -1,5 +1,5 @@
 export const uri = (process.env.NODE_ENV === 'production') ?
-  'http://ec2-54-244-27-45.us-west-2.compute.amazonaws.com:3333/' :
+  'http://ec2-54-244-27-45.us-west-2.compute.amazonaws.com:3333' :
   'http://localhost:3333';
 
 export default { uri };
